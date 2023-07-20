95db854a7a041b3dd5d56c4deda170fd061d945b
Maria Jazmin <jazminbarreto19@gmail.com>
2023-07-20 18:22:26 -0400

    update4.1

--- fundamentals/oops/usuario_I.py
+++ fundamentals/oops/usuario_I.py
@@ -30,14 +30,21 @@ class User:
 # Ejemplo de uso
 user1 = User("Maria Jazmin Barreto", "jazbarreto@python.com")
 user2 = User("Cleopatra Python", "cleo@python.com")
+user3 = User("Anubis Python", "anubis@python.com")
 
 user1.make_deposit(200)  # Depósito de $200 en user1
 user1.make_deposit(50)   # Depósito adicional de $50 en user1
-user1.show_user_balance()  # Muestra el balance del user1
-
-user1.withdraw_money(100)  # Retira $100 de user1
-user1.show_user_balance()  # Muestra el balance actualizado de user1
-
+user1.make_deposit(350)   # Depósito adicional de $350 en user1
 user1.transfer_money(user2, 50)  # Transfiere $50 de user1 a user2
 user1.show_user_balance()  # Muestra el balance actualizado de user1
+
+user2.make_deposit(150)   # Depósito de $150 en user2
+user2.make_deposit(50)   # Depósito adicional de $50 en user2
+user2.transfer_money(user1, 50)  # Transfiere $50 de user2 a user1
+user2.transfer_money(user1, 50)  # Transfiere $50 de user2 a user1
 user2.show_user_balance()  # Muestra el balance actualizado de user2
+
+user3.make_deposit(650)   # Depósito de $650 en user3
+user3.transfer_money(user1, 150)  # Transfiere $150 de user3 a user1
+user3.transfer_money(user2, 350)  # Transfiere $350 de user3 a user2
+user3.show_user_balance()  # Muestra el balance actualizado de user3
