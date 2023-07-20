8ce4538767bc4f7091a334fca9d07263a553a1fc
Maria Jazmin <jazminbarreto19@gmail.com>
2023-07-20 19:25:04 -0400

    bank_account

--- fundamentals/oops/bank_account.py
+++ fundamentals/oops/bank_account.py
@@ -5,6 +5,7 @@ class CuentaBancaria:
 
     def deposito(self, amount):
         self.balance += amount
+        return self
 
     def retiro(self, amount):
         if self.balance >= amount:
@@ -12,12 +13,16 @@ class CuentaBancaria:
         else:
             print("Fondos insuficientes: cobrando una tarifa de $5")
             self.balance -= 5
+        return self
+
     def mostrar_info_cuenta(self):
         print(f"Balance: ${self.balance}")
+        return self
 
     def generar_interes(self):
         if self.balance > 0:
             self.balance += self.balance * self.interes
+        return self
 
 # Crear dos cuentas bancarias
 cuenta1 = CuentaBancaria(interes=0.01, balance=1000)
--- fundamentals/oops/practica.py
+++ fundamentals/oops/practica.py
@@ -17,20 +17,57 @@
 # print(guido.name)	# salida: Michael
 # print(monty.name)	# salida: Michael
 
-class Usuario:
-    # Atributo de clase
+# class Usuario:
+#     # Atributo de clase
+#     nombre_banco = "Primer Dojo Nacional"
+
+#     # Constructor
+#     def __init__(self, name, email_address):
+#         # Atributos de instancia
+#         self.name = name
+#         self.email = email_address
+#         self.balance_cuenta = 0
+
+# def make_deposit(self, amount):	# toma un argumento que es el monto del depósito
+#         self.balance_cuenta += amount	# la cuenta del usuario específico aumenta en la cantidad del valor recibido
+# # método de clase para cambiar el nombre del banco
+
+class CuentaBancaria:
     nombre_banco = "Primer Dojo Nacional"
+    todas_las_cuentas = []
 
-    # Constructor
-    def __init__(self, name, email_address):
-        # Atributos de instancia
-        self.name = name
-        self.email = email_address
-        self.balance_cuenta = 0
+    def __init__(self, int_rate, balance):
+        self.tasa_int = int_rate
+        self.balance = balance
+        CuentaBancaria.todas_las_cuentas.append(self)
 
-def make_deposit(self, amount):	# toma un argumento que es el monto del depósito
-        self.balance_cuenta += amount	# la cuenta del usuario específico aumenta en la cantidad del valor recibido
+    def re_tiro(self, amount):
+        # Podemos usar el método estático aquí para evaluar
+        # si podemos retirar los fondos sin quedar con balance negativo
+        if CuentaBancaria.puede_retirar(self.balance, amount):
+            self.balance -= amount
+        else:
+            print("Fondos insuficientes")
+        return self
 
+    # Los métodos estáticos no tienen acceso a ningún atributo
+    # solo a lo que se le pasa
+    @staticmethod
+    def puede_retirar(balance, amount):
+        if (balance - amount) < 0:
+            return False
+        else:
+            return True
 
+    @classmethod
+    def cambiar_nombre_banco(cls, name):
+        cls.nombre_banco = name
+
+    @classmethod
+    def todos_los_balances(cls):
+        sum = 0
+        for account in cls.todas_las_cuentas:
+            sum += account.balance
+        return sum
 
 
